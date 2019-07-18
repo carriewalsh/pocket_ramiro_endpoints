244d3249a24526a366eb0afea7a825eebd315834
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-17 21:56:41 -0600

    Adds footer and header

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './stylesheets/Footer.css'
+
+export default function Header() {
+  return (
+    <footer>
+      <div class="three-fractions salmon"></div>
+      <div class="three-fractions purple"></div>
+      <div class="two-fractions yellow"></div>
+      <div class="one-fraction pink"></div>
+      <div class="two-fractions mint"></div>
+    </footer>
+  )
+}
