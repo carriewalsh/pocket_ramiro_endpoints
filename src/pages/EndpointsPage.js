26ae03535584d7d1741a5d6e1f5917e6e86d2594
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-18 13:12:18 -0600

    Adds endpoints for Resources

--- src/pages/EndpointsPage.js
+++ src/pages/EndpointsPage.js
@@ -12,7 +12,7 @@ class EndpointsPage extends Component {
           id: 1,
           route: 'GET /api/v1/resources',
           request: `credentials`,
-          response: `[{ \nid: 1, \nname: 'Cameron Clarks', \nemail: 'example@example.com', \npassword_digest: 'qp34uchon8w47yabov8ezfhpv', \nphone_number: 5555555555, \nrole: 'admin', \nactive: true, \ncreated_at: '2019-07-16T21:52:00.823Z', \nupdated_at: '2019-07-16T21:52:00.823Z' \n}] \n`,
+          response: `[{ \nid: 1, \nname: Cameron Clarks, \nemail: example@example.com, \npassword_digest: qp34uchon8w47yabov8ezfhpv, \nphone_number: 5555555555, \nrole: admin, \nactive: true, \ncreated_at: 2019-07-16T21:52:00.823Z, \nupdated_at: 2019-07-16T21:52:00.823Z \n}] \n`,
           color: "mint",
           key: 1
         },
@@ -20,9 +20,25 @@ class EndpointsPage extends Component {
           id: 2,
           route: 'GET /api/v1/resources/:id',
           request: `credentials`,
-          response: ` {\n id: 1,\n name: 'Cameron Clarks',\n email: 'example@example.com',\n password_digest: 'qp34uchon8w47yabov8ezfhpv',\n phone_number: 5555555555,\n role: 'admin',\n active: true,\n created_at: '2019-07-16T21:52:00.823Z',\n updated_at: '2019-07-16T21:52:00.823Z'\n }\n`,
+          response: ` {\n id: 1,\n name: Cameron Clarks,\n email: example@example.com,\n password_digest: qp34uchon8w47yabov8ezfhpv,\n phone_number: 5555555555,\n role: admin,\n active: true,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }\n`,
           color: "mint",
-          key: 1
+          key: 2
+        },
+        {
+          id: 3,
+          route: 'POST Resources /api/v1/resources',
+          request: `{\n resource_type_id: 1,\n name: Bright Tank 3,\n cost: 5091,\n user_id: 1,\n active: TRUE,\n created_at: 2019-'07-16T21:52:00.823Z',\n updated_at: '2019-07-16T21:52:00.823Z'\n}`,
+          response: `{\n id:2,\n machine_type_id: 1,\n user_id: 1,\n name: Bright Tank 2,\n cost: 5091,\n active: TRUE,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }`,
+          color: "mint",
+          key: 3
+        },
+        {
+          id: 4,
+          route: 'PATCH Resources /api/v1/resources/:id',
+          request: `{\n name: Bright Tank \n }`,
+          response: `{\n id:3,\n machine_type_id: 1,\n user_id: 1,\n name: Bright Tank 4,\n cost: 5091,\n active: TRUE,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }`,
+          color: "mint",
+          key: 4
         }
       ]
     }
