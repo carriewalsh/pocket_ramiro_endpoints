1910b81a1becf7d435d6107be39dd9b1b5683fca
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-18 14:25:39 -0600

    Adds the rest of the endpoints

--- src/pages/EndpointsPage.js
+++ src/pages/EndpointsPage.js
@@ -12,7 +12,7 @@ class EndpointsPage extends Component {
           id: 1,
           route: 'GET /api/v1/resources',
           request: `credentials`,
-          response: `[{ \nid: 1, \nname: Cameron Clarks, \nemail: example@example.com, \npassword_digest: qp34uchon8w47yabov8ezfhpv, \nphone_number: 5555555555, \nrole: admin, \nactive: true, \ncreated_at: 2019-07-16T21:52:00.823Z, \nupdated_at: 2019-07-16T21:52:00.823Z \n}] \n`,
+          response: `[{\n id:1,\n machine_type_id: 1,\n user_id: 1,\n name: Bright Tank 1,\n cost: 5000,\n active: TRUE,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n },\n {\n id:2,\n machine_type_id: 1,\n user_id: 1,\n name: Bright Tank 2,\n cost: 5091,\n active: TRUE,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }]`,
           color: "mint",
           key: 1
         },
@@ -20,13 +20,13 @@ class EndpointsPage extends Component {
           id: 2,
           route: 'GET /api/v1/resources/:id',
           request: `credentials`,
-          response: ` {\n id: 1,\n name: Cameron Clarks,\n email: example@example.com,\n password_digest: qp34uchon8w47yabov8ezfhpv,\n phone_number: 5555555555,\n role: admin,\n active: true,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }\n`,
+          response: `{\n id:2,\n machine_type_id: 1,\n user_id: 1,\n name: Bright Tank 2,\n cost: 5091,\n active: TRUE,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }`,
           color: "mint",
           key: 2
         },
         {
           id: 3,
-          route: 'POST Resources /api/v1/resources',
+          route: 'POST /api/v1/resources',
           request: `{\n resource_type_id: 1,\n name: Bright Tank 3,\n cost: 5091,\n user_id: 1,\n active: TRUE,\n created_at: 2019-'07-16T21:52:00.823Z',\n updated_at: '2019-07-16T21:52:00.823Z'\n}`,
           response: `{\n id:2,\n machine_type_id: 1,\n user_id: 1,\n name: Bright Tank 2,\n cost: 5091,\n active: TRUE,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }`,
           color: "mint",
@@ -34,12 +34,173 @@ class EndpointsPage extends Component {
         },
         {
           id: 4,
-          route: 'PATCH Resources /api/v1/resources/:id',
+          route: 'PATCH /api/v1/resources/:id',
           request: `{\n name: Bright Tank \n }`,
           response: `{\n id:3,\n machine_type_id: 1,\n user_id: 1,\n name: Bright Tank 4,\n cost: 5091,\n active: TRUE,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }`,
           color: "mint",
           key: 4
+        },
+        {
+          id: 5,
+          route: 'GET /api/v1/resource_types',
+          request: `credentials`,
+          response: `[{\n id:1,\n category: Bright Tank,\n company: Tanks.com,\n contact_number: 444-444-4444,\n contact_name: John Johnson,\n active: true,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n },\n {\n id:2,\n category: Van,\n company: Chevrolet,\n contact_number: 333-333-3333,\n contact_name: John Johnsonite,\n active: true,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }]`,
+          color: "salmon",
+          key: 5
+        },
+        {
+          id: 6,
+          route: 'GET /api/v1/resource_types/:id',
+          request: `credentials`,
+          response: `{\n id:1,\n category: Bright Tank,\n company: Tanks.com,\n contact_number: 444-444-4444,\n contact_name: John Johnson,\n active: true,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }`,
+          color: "salmon",
+          key: 6
+        },
+        {
+          id: 7,
+          route: 'POST /api/v1/resource_types/:id',
+          request: `{\n category: Bright Tank,\n company: Tanks.com,\n contact_number: 444-444-4444,\n contact_name: John Johnson\n }`,
+          response: `{\n id:1,\n category: Bright Tank,\n company: Tanks.com,\n contact_number: 444-444-4444,\n contact_name: John Johnson,\n active: true,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }`,
+          color: "salmon",
+          key: 7
+        },
+        {
+          id: 8,
+          route: 'PATCH /api/v1/resource_types/:id',
+          request: `{\n company: Tanks.com\n }`,
+          response: `{\n id:1,\n category: Bright Tank,\n company: Tanks.gov,\n contact_number: 444-444-4444,\n contact_name: John Johnson,\n active: true,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }`,
+          color: "salmon",
+          key: 8
+        },
+        {
+          id: 9,
+          route: 'GET /api/v1/\nresources/:resource_id/parts',
+          request: `credentials`,
+          response: `[{\n id: 1,\n name: lever 2000,\n inventory: 1,\n active: true,\n created_at: 2019-07-16T13:15:31.885Z,\n updated_at: 2019-07-16T13:16:16.146Z\n },\n {\n id: 2,\n name: Tank patch,\n inventory: 1,\n active: true,\n created_at: 2019-07-16T13:18:09.048Z,\n updated_at: 2019-07-16T13:18:09.048Z\n }]`,
+          color: "yellow",
+          key: 9
+        },
+        {
+          id: 10,
+          route: 'GET /api/v1/\nresources/:resource_id/parts/:id',
+          request: `credentials`,
+          response: `{\n id: 1,\n name: lever 2000,\n inventory: 1,\n active: true,\n created_at: 2019-07-16T13:15:31.885Z,\n updated_at: 2019-07-16T13:16:16.146Z\n }`,
+          color: "yellow",
+          key: 10
+        },
+        {
+          id: 11,
+          route: 'POST /api/v1/\nresources/:resource_id/parts',
+          request: `{\n name: lever 2000,\n inventory: 1,\n }`,
+          response: `{\n id: 1,\n name: lever 2000,\n inventory: 1,\n active: true,\n created_at: 2019-07-16T13:15:31.885Z,\n updated_at: 2019-07-16T13:16:16.146Z\n }`,
+          color: "yellow",
+          key: 11
+        },
+        {
+          id: 12,
+          route: 'PATCH /api/v1/\nresources/:resource_id/parts/:id',
+          request: `{\n name: lever 4000\n }`,
+          response: `{\n id: 1,\n name: lever 4000,\n inventory: 1,\n active: true,\n created_at: 2019-07-16T13:15:31.885Z,\n updated_at: 2019-07-19T13:16:16.146Z\n }`,
+          color: "yellow",
+          key: 12
+        },
+        {
+          id: 13,
+          route: 'GET /api/v1/\nresources/:resource_id/tickets',
+          request: `credentials`,
+          response: `[{\n id: 1,\n table_key: 1,\n table_name: Resources,\n user_id: 1,\n notes: needs oil change,\n priority: high,\n frequency_unit: null,\n frequency_value: null,\n created_at: 2019-07-14T17:02:17.064Z,\n updated_at: 2019-07-14T17:02:17.064Z,\n active: true\n }]`,
+          color: "purple",
+          key: 13
+        },
+        {
+          id: 14,
+          route: 'GET /api/v1/\nresources/:resource_id/tickets/:id',
+          request: `credentials`,
+          response: `{\n id: 1,\n table_key: 1,\n table_name: Resources,\n user_id: 1,\n notes: needs oil change,\n priority: high,\n frequency_unit: null,\n frequency_value: null,\n created_at: 2019-07-14T17:02:17.064Z,\n updated_at: 2019-07-14T17:02:17.064Z,\n active: true\n }`,
+          color: "purple",
+          key: 14
+        },
+        {
+          id: 15,
+          route: 'POST /api/v1/\nresources/:resource_id/tickets',
+          request: `{\n table_key: 1,\n table_name: Resources,\n user_id: 1,\n notes: needs oil change,\n priority: high,\n frequency_unit: null,\n frequency_value: null\n }`,
+          response: `{\n id: 1,\n table_key: 1,\n table_name: Resources,\n user_id: 1,\n notes: needs oil change,\n priority: high,\n frequency_unit: null,\n frequency_value: null,\n created_at: 2019-07-14T17:02:17.064Z,\n updated_at: 2019-07-14T17:02:17.064Z,\n active: true\n }`,
+          color: "purple",
+          key: 15
+        },
+        {
+          id: 16,
+          route: 'PATCH /api/v1/\nresources/:resource_id/tickets/:id',
+          request: `{\n priority: low\n }`,
+          response: `{\n id: 1,\n table_key: 1,\n table_name: Resources,\n user_id: 1,\n notes: needs oil change,\n priority: low,\n frequency_unit: null,\n frequency_value: null,\n created_at: 2019-07-14T17:02:17.064Z,\n updated_at: 2019-07-14T17:02:17.064Z,\n active: true\n }`,
+          color: "purple",
+          key: 16
+        },
+        {
+          id: 17,
+          route: 'GET /api/v1/tickets',
+          request: `credentials`,
+          response: `[{\n id: 1,\n table_key: 1,\n table_name: Resources,\n user_id: 1,\n notes: needs oil change,\n priority: high,\n frequency_unit: null,\n frequency_value: null,\n created_at: 2019-07-14T17:02:17.064Z,\n updated_at: 2019-07-14T17:02:17.064Z,\n active: true\n }]`,
+          color: "purple",
+          key: 17
+        },
+        {
+          id: 18,
+          route: 'GET /api/v1/\nresources/:resource_id/notes',
+          request: `credentials`,
+          response: `[{\n id: 1,\n user_id: 1,\n table_key: 1,\n table_name: Resources,\n magna aliqua.,\n active: true,\n created_at: 2019-07-14T17:02:17.064Z,\n updated_at: 2019-07-14T17:02:17.064Z,\n },\n {\n id: 2,\n user_id: 1,\n table_key: 1,\n table_name: Resources,\n magna aliqua.,\n active: true,\n created_at: 2019-07-14T17:02:17.064Z,\n updated_at: 2019-07-14T17:02:17.064Z,\n }]`,
+          color: "pink",
+          key: 18
+        },
+        {
+          id: 19,
+          route: 'GET /api/v1/\nresources/:resource_id/notes/:id',
+          request: `credentials`,
+          response: `{\n id: 1,\n user_id: 1,\n table_key: 1,\n table_name: Resources,\n magna aliqua.,\n active: true,\n created_at: 2019-07-14T17:02:17.064Z,\n updated_at: 2019-07-14T17:02:17.064Z,\n }`,
+          color: "pink",
+          key: 19
+        },
+        {
+          id: 20,
+          route: 'POST /api/v1/\nresources/:resource_id/notes',
+          request: `{\n user_id: 1,\n table_key: 1,\n table_name: Resources,\n magna aliqua.,\n }`,
+          response: `{\n id: 1,\n user_id: 1,\n table_key: 1,\n table_name: Resources,\n magna aliqua.,\n active: true,\n created_at: 2019-07-14T17:02:17.064Z,\n updated_at: 2019-07-14T17:02:17.064Z,\n }`,
+          color: "pink",
+          key: 20
+        },
+        {
+          id: 21,
+          route: 'PATCH /api/v1/\nresources/:resource_id/notes/:id',
+          request: `{\n content: This machine is cranky.\n `,
+          response: `{\n id: 2,\n user_id: 1,\n table_key: 1,\n table_name: Resources,\n magna aliqua.,\n active: true,\n created_at: 2019-07-14T17:02:17.064Z,\n updated_at: 2019-07-14T17:02:17.064Z,\n }`,
+          color: "pink",
+          key: 21
+        },
+        {
+          id: 22,
+          route: 'GET /api/v1/users/:id',
+          request: `credentials`,
+          response: `[{ \nid: 1, \nname: Cameron Clarks, \nemail: example@example.com, \npassword_digest: qp34uchon8w47yabov8ezfhpv, \nphone_number: 5555555555, \nrole: admin, \nactive: true, \ncreated_at: 2019-07-16T21:52:00.823Z, \nupdated_at: 2019-07-16T21:52:00.823Z \n}] \n`,
+          color: "orange",
+          key: 22
+        },
+        {
+          id: 23,
+          route: 'POST /api/v1/users',
+          request: `{\n name: Cameron Clarks,\n email: example@example.com,\n password: password,\n password_confirmation: password,\n phone_number: 555-555-5555,\n role: admin\n }`,
+          response: `{\n id: 1,\n name: Cameron Marks,\n email: example@example.com,\n password_digest: qp34uchon8w47yabov8ezfhpv,\n phone_number: 555-555-5555,\n role: admin,\n active: true,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }`,
+          color: "orange",
+          key: 23
+        },
+        {
+          id: 24,
+          route: 'PATCH /api/v1/users/:id',
+          request: `{\n phone_number: 777-777-7777\n }`,
+          response: `{\n id: 1,\n name: Cameron Marks,\n email: example@example.com,\n password_digest: qp34uchon8w47yabov8ezfhpv,\n phone_number: 777-777-7777,\n role: admin,\n active: true,\n created_at: 2019-07-16T21:52:00.823Z,\n updated_at: 2019-07-16T21:52:00.823Z\n }`,
+          color: "orange",
+          key: 24
         }
+
       ]
     }
   }
